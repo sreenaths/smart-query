a865f7cbdaf7de97084443647e082419b8dc71ae
sreenaths <somarajapuram@gmail.com>
2023-05-04 16:58:15 -0700

    [#14] Server DB schema - Exception handling in schema api

--- server/src/util/db.py
+++ server/src/util/db.py
@@ -18,9 +18,14 @@ def get_schema(connector_id, db_name):
 
     schema = []
     for table_name in table_names:
+      try:
+        details = db.get_table_info_no_throw([table_name]).strip()
+      except Exception as e:
+        details = f'Error: loading details of {table_name} failed.\n\n{e}'
+        print(details)
+
       schema.append({
         "table_name": table_name,
-        "details": db.get_table_info_no_throw([table_name])
+        "details": details
       })
-
     return schema
