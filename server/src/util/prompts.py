3f1b91bf147dfbae677c9fe3b95798edc4754d9c
sreenaths <somarajapuram@gmail.com>
2023-05-07 15:00:32 -0700

    [#15, #16] Improving prompts for Generation & Summarise

--- server/src/util/prompts.py
+++ server/src/util/prompts.py
@@ -11,22 +11,23 @@ Thought: I should look at the tables in the database to see what I can query.
 
 _GENERATE_SUFFIX = """
 Current database is {db_name}.
-If table details are unknown, you could look at the tables in the database.
+If table details are unknown, you can look at the tables in the database.
 
-At the end don't return the final result. Only return the {dialect} SQL query that answers the input question.
+Carefully analyse the input question, and at the end don't return an answer in english. Final Answer must be a valid SQL query in {dialect} dialect that can be used to answer the input question.
 
 Begin!
 
-Question: {input}
+Input Question: {input}
 Thought: How do I create the most simple {dialect} SQL query.
 {agent_scratchpad}"""
 
 _SUMMARISE_SUFFIX = """
 Current database is {db_name}.
 
-Summarise what the following query does.
+Summarise what the following SQL query does in simple words without executing it.
+The SQL query is in {dialect} dialect.
 
-Query: {input}
+SQL query: {input}
 Thought: I should look at the tables in the database to see what tables are available.
 {agent_scratchpad}"""
 
